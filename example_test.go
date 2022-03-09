64e8347336d6e090fe755aa6c1f47053417e507f
Roger Guldbrandsen <roger.guldbrandsen@gmail.com>
2022-03-09 13:31:18 +0900

    docs: Add runnable example

---
+++ example_test.go
@@ -0,0 +1,41 @@
+package rogerr_test
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/kinbiko/rogerr"
+)
+
+func ExampleWrap() {
+	someFuncWithAProblem := func(ctx context.Context) error {
+		return fmt.Errorf("some low level err")
+	}
+
+	someFuncThatWrapsWithRogerr := func(ctx context.Context) error {
+		// Attach some projectID to the context as structured metadata
+		ctx = rogerr.WithMetadatum(ctx, "projectID", 123)
+
+		err := someFuncWithAProblem(ctx)
+		if err != nil {
+			return rogerr.Wrap(ctx, err, "wrap args")
+		}
+		return nil
+	}
+
+	someFuncThatWrapsARogerrError := func(ctx context.Context) error {
+		err := someFuncThatWrapsWithRogerr(ctx)
+		if err != nil {
+			return fmt.Errorf("wrap with fmt: %w", err)
+		}
+		return nil
+	}
+
+	err := someFuncThatWrapsARogerrError(context.Background())
+	md := rogerr.Metadata(err)
+	fmt.Println(err.Error())     // error message should be cleanly wrapped
+	fmt.Println(md["projectID"]) // structured metadata should be available
+	//output:
+	// wrap with fmt: wrap args: some low level err
+	// 123
+}
